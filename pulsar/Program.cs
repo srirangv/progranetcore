13dd95ccec12c096de6cac2b8457dbfadfaf10ae
Vikram Srirangam <vsrirangam@wingstop.com>
2021-12-21 16:07:15 +0530

    initial commit

---
+++ pulsar/Program.cs
@@ -0,0 +1,21 @@
+﻿using Prometheus;
+using System;
+using System.Threading;
+
+class Program
+{
+    private static readonly Counter TickTock =
+        Metrics.CreateCounter("sampleapp_ticks_total", "Just keeps on ticking");
+
+    static void Main()
+    {
+        var server = new MetricServer(port: 1234);
+        server.Start();
+
+        while (true)
+        {
+            TickTock.Inc();
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+        }
+    }
+}
